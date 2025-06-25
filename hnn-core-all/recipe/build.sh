4c0533e45e0116ef32e7b65918eeb41868e31403
Austin E. Soplata <me@asoplata.com>
2025-06-25 14:27:48 -0400

    Update NRN to 8.2.7 and HNN to 0.4.2

--- hnn-core-all/recipe/build.sh
+++ hnn-core-all/recipe/build.sh
@@ -7,9 +7,9 @@ echo ""
 
 if [ `uname` == Darwin ]; then
     if [ `uname -m` == arm64 ]; then
-        NRN_WHL_URL=https://files.pythonhosted.org/packages/76/89/4e659723194edb3351a37b60a474843e68f676bc983c41047c234b544494/NEURON-8.2.6-cp312-cp312-macosx_11_0_arm64.whl
+        NRN_WHL_URL=https://files.pythonhosted.org/packages/fe/d0/a22eceadcb655aa03a8a37f6e109e0880a7d00978713f0f5ded8042cfb5f/neuron-8.2.7-cp312-cp312-macosx_11_0_arm64.whl
     elif [ `uname -m` == x86_64 ]; then
-        NRN_WHL_URL=https://files.pythonhosted.org/packages/d7/52/50ae4bf3dcc87cf71daa559f1b188b4a8e85f7f19073d976eb12821f8692/NEURON-8.2.6-cp312-cp312-macosx_10_15_x86_64.whl
+        NRN_WHL_URL=https://files.pythonhosted.org/packages/ab/45/26a87553e7a1dde05fb9aebe98715498b6273fd0ed08f5a82ad550ee5379/neuron-8.2.7-cp312-cp312-macosx_10_15_x86_64.whl
     fi
     # Adapted from https://docs.conda.io/projects/conda-build/en/latest/resources/activate-scripts.html
     for CHANGE in "activate" "deactivate"
@@ -20,8 +20,9 @@ if [ `uname` == Darwin ]; then
 fi
 
 if [ `uname` == Linux ]; then
+    # Note that as of NEURON 8.2.7 (checked 2025-06-24 at https://pypi.org/project/NEURON/8.2.7/#files ), NEURON are no longer distributing Linux ARM wheels
     if [ `uname -m` == x86_64 ]; then
-        NRN_WHL_URL=https://files.pythonhosted.org/packages/14/4e/e0c65911a59b646274ba4f6740e8705ff29863879b0a629e92666d682ebd/NEURON-8.2.6-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+        NRN_WHL_URL=https://files.pythonhosted.org/packages/3e/69/5a8d498fd3096726768ab875b0e9a633cfdb68f976a6d520b6158b07ed7c/neuron-8.2.7-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
     fi
     # Adapted from https://docs.conda.io/projects/conda-build/en/latest/resources/activate-scripts.html
     for CHANGE in "activate" "deactivate"
